2cab212be80bff66a70a77972de8b5509d5e0bff
imran752310 <muhammadimran752310@gmail.com>
2025-05-09 16:56:27 +0500

    starting

---
+++ components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './Hero/Hero'
+import About from './About/About'
+import OurPartner from './Partner/OurPartner'
+import Services from './Services/Services'
+import Testimonial from './Testimonial/Testimonial'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+        <About />
+        <OurPartner />
+        <Services />
+        <Testimonial />
+    </div>
+  )
+}
+
+export default Home
---
+++ components/Home/Partner/OurPartner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OurPartner = () => {
+  return (
+    <div>OurPartner</div>
+  )
+}
+
+export default OurPartner
