0059683a5bb5c2d74c13deb53ea026ec58676290
imran752310 <muhammadimran752310@gmail.com>
2025-05-10 23:17:41 +0500

    our partner

--- components/Home/Partner/OurPartner.tsx
+++ components/Home/Partner/OurPartner.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import PartnerSlider from '../Partner/Slider'
 
 const OurPartner = () => {
   return (
-    <div>OurPartner</div>
+      <div className="my-14">
+        <div className="text-center">
+        <h2 className="text-purple-700 text-md font-bold">WHO WE ARE</h2>
+        <h1 className="text-4xl font-bold mt-2">rusted by 25,000+ world-class brands <br /> and organizations of all sizes</h1>
+        <div>
+        <PartnerSlider />
+        
+        </div>
+        </div>
+       </div>
   )
 }
 
---
+++ components/Home/Partner/Slider.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Image from "next/image";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+const slides = [
+  "/image/logo1.webp",
+  "/image/logo2.webp",
+  "/image/logo3.webp",
+  "/image/logo4.webp",
+];
+
+const SwiperCarousel = () => {
+  return (
+    <div className="w-[90%] mx-auto my-10">
+      <Swiper
+        spaceBetween={20}
+        centeredSlides={true}
+        autoplay={{ delay: 4000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        navigation={true}
+        modules={[Autoplay]}
+        breakpoints={{
+            640: { slidesPerView: 2 }, // Show 2 logos on tablets
+            1024: { slidesPerView: 4 }, // Show 4 logos on desktop
+        }}
+        className="mySwiper"
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="flex justify-center">
+              <Image
+                src={slide}
+                alt={`Slide ${index + 1}`}
+                width={100}
+                height={100}
+                className=""
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default SwiperCarousel;
