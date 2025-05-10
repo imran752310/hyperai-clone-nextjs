9acf06c1e2f93d185ab8fc5d056eadfb96d9d72c
imran752310 <muhammadimran752310@gmail.com>
2025-05-10 23:06:23 +0500

    hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Image from "next/image";
+import { GoArrowUpRight } from "react-icons/go";
+import { IoIosPlay } from "react-icons/io";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <section className="w-[90%] h-[600] mx-auto  relative flex flex-col md:flex-row items-center justify-between bg-gradient-to-r from-blue-500 to-purple-500 text-white p-10 md:p-20 rounded-3xl">
+            <div className="md:w-1/2">
+                <span className="bg-purple-700 px-4 py-1 text-sm">Future of Marketing is Here</span>
+                <h1 className="text-4xl md:text-5xl font-bold mt-4">
+                    Let AI Take Your Business To The Next Level
+                </h1>
+                <p className="mt-4 text-lg">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                    tellus, luctus nec ullamcorper mattis.
+                </p>
 
-export default Hero
+                <div className="mt-8 flex flex-col sm:flex-row sm:justify-center lg:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
+                    <button className="bg-purple-700 text-white px-6 flex items-center rounded-sm hover:bg-purple-800 hover:text-white transition hover:animate-pulse">Get a Started
+                        <GoArrowUpRight />
+                    </button>
+                    <a href="#" className="flex items-center text-red border-2 border-white p-5 rounded-full hover:bg-white hover:text-purple-700 transition hover:animate-pulse hover:shadow-slate-100">
+                        <IoIosPlay className=" " />
+                    </a>
+                </div>
+            </div>
+            <div className="mt-6 md:mt-0 md:w-1/2 flex justify-center">
+                <Image src="/image/hero.webp" width={500} height={500} alt="AI User" className="rounded-lg" />
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
