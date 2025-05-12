49ccb922c941a15079b879b1f42e99130e2ad25a
imran752310 <muhammadimran752310@gmail.com>
2025-05-12 22:56:01 +0500

    testimonial Card

--- components/Home/Testimonial/Testimonial.tsx
+++ components/Home/Testimonial/Testimonial.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import TestimonialCarousel from './TestimonialCard'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className="my-14">
+        <div className="text-center">
+        <h2 className="text-purple-700 text-md font-bold">Tesimonials</h2>
+        <h1 className="text-4xl font-bold mt-2">What Our Clients Say <br /> About HyperAI</h1>
+        <div>
+      <TestimonialCarousel />
+        
+        </div>
+        </div>
+       </div>
   )
 }
 
